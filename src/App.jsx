f5094ecf3f227994449466a275d087e4f215bdf1
Sahil Agrawal <hellocodesahil@gmail.com>
2024-06-28 15:03:17 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+// src/App.jsx
+import React from 'react';
+import Calculator from './components/Calculator';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="app">
+      <Calculator />
+    </div>
+  );
+};
+
+export default App;
